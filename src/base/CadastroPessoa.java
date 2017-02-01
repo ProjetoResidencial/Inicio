27f61ac89f5795eaef8a73b971ab814c0919ad5c
henriquefranzoni <henrique_franzoni@hotmail.com>
2017-02-01 00:52:02 -0200

    Criação da classe Cadastro de Pessoa, seus atributos para o gerenciamento de contas.

---
+++ src/base/CadastroPessoa.java
@@ -0,0 +1,15 @@
+package base;
+
+public class CadastroPessoa {
+	
+	String nome;
+	String EstadoCivil;
+	String Cidade;
+	String Endereco;
+	String Estado;
+	String Rg;
+	String Cpf;
+	int idade;
+	
+
+}
